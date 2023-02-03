1664338f3b2add40be82b6dd56ee6e06d56aee9b
charliebrockmeier <cbrockmeier2@huskers.unl.edu>
2023-02-03 12:34:11 -0600

    Changed Gitlab to use a helper class

--- src/edu/unl/raikes/git/GitLab2023.java
+++ src/edu/unl/raikes/git/GitLab2023.java
@@ -2,6 +2,7 @@ package edu.unl.raikes.git;
 
 public class GitLab2023 {
     public static void main(String[] args) {
-        System.out.print("hi");
+        GitLabHelper helper = new GitLabHelper("Charlie", "Madelyn");
+        helper.PrintWelcomeMessage();   
     }
 }
